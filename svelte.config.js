f3c712046286d6b632fd1297cf42e01ee91a134c
Spikey Sanju <spikeysanju98@gmail.com>
2022-09-25 21:00:16 +0530

    build: :rocket: Deploy to cloudflare!

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-cloudflare";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
 
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	],
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+  },
 };
 
 export default config;
