6958a12f4299355652db5743e447faaba827b0e0
Trishiraj <gamerprince.exp@gmail.com>
2022-10-05 04:54:46 +0530

    Feature: Add custom cursor effect style
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,85 @@
   import Speaker from '$lib/section/speaker/speaker.svelte';
   import Tickets from '$lib/section/tickets/Tickets.svelte';
   import Why from '$lib/section/why/Why.svelte';
+  import { onMount } from 'svelte';
+
+onMount(() => {
+    const lerp = (a, b, n) => (1 - n) * a + n * b;
+    const body = document.body;
+    const getMousePos = (e) => {
+        let posx = 0;
+        let posy = 0;
+        if (!e) e = window.event;
+        if (e.pageX || e.pageY) {
+            posx = e.pageX;
+            posy = e.pageY;
+        }
+        else if (e.clientX || e.clientY) 	{
+            posx = e.clientX + body.scrollLeft + document.documentElement.scrollLeft;
+            posy = e.clientY + body.scrollTop + document.documentElement.scrollTop;
+        }
+        return { x : posx, y : posy }
+    }
+
+    // Window sizes.
+    let winsize;
+    const calcWinsize = () => winsize = {width: window.innerWidth, height: window.innerHeight};
+    calcWinsize();
+    // Recalculate window sizes on resize.
+    window.addEventListener('resize', calcWinsize);
+
+    // Custom mouse cursor.
+    class CursorFx {
+        constructor(el) {
+            this.DOM = {el: el};
+            this.DOM.dot = this.DOM.el.querySelector('.cursor__inner--dot');
+            this.DOM.circle = this.DOM.el.querySelector('.cursor__inner--circle');
+            this.bounds = {dot: this.DOM.dot.getBoundingClientRect(), circle: this.DOM.circle.getBoundingClientRect()};
+            this.scale = 1;
+            this.opacity = 1;
+            this.mousePos = {x:0, y:0};
+            this.lastMousePos = {dot: {x:0, y:0}, circle: {x:0, y:0}};
+            this.lastScale = 1;
+            this.lastOpacity = 1;
+            
+            this.initEvents();
+            requestAnimationFrame(() => this.render());
+        }
+        initEvents() {
+            window.addEventListener('mousemove', ev => this.mousePos = getMousePos(ev));
+        }
+        render() {
+            this.lastMousePos.dot.x = lerp(this.lastMousePos.dot.x, this.mousePos.x - this.bounds.dot.width/2, 1);
+            this.lastMousePos.dot.y = lerp(this.lastMousePos.dot.y, this.mousePos.y - this.bounds.dot.height/2, 1);
+            this.lastMousePos.circle.x = lerp(this.lastMousePos.circle.x, this.mousePos.x - this.bounds.circle.width/2, 0.15);
+            this.lastMousePos.circle.y = lerp(this.lastMousePos.circle.y, this.mousePos.y - this.bounds.circle.height/2, 0.15);
+            this.lastScale = lerp(this.lastScale, this.scale, 0.15);
+            this.lastOpacity = lerp(this.lastOpacity, this.opacity, 0.1);
+            this.DOM.dot.style.transform = `translateX(${(this.lastMousePos.dot.x)}px) translateY(${this.lastMousePos.dot.y}px)`;
+            this.DOM.circle.style.transform = `translateX(${(this.lastMousePos.circle.x)}px) translateY(${this.lastMousePos.circle.y}px) scale(${this.lastScale})`;
+            this.DOM.circle.style.opacity = this.lastOpacity
+            requestAnimationFrame(() => this.render());
+        }
+        enter() {
+            cursor.scale = 2.7;
+        }
+        leave() {
+            cursor.scale = 1;
+        }
+        click() {
+            this.lastScale = 1;
+            this.lastOpacity = 0;
+        }
+    }
+    
+    const cursor = new CursorFx(document.querySelector('.cursor'));
+
+    [...document.querySelectorAll('button, a')].forEach((link) => {
+        link.addEventListener('mouseenter', () => cursor.enter() );
+        link.addEventListener('mouseleave', () => cursor.leave() );
+        link.addEventListener('click', () => cursor.click() );
+    });
+  });
 </script>
 
 <svelte:head>
@@ -22,6 +101,10 @@
 <div
   class="flex flex-col items-center justify-start space-y-16 md:space-y-44 p-8 min-h-screen overflow-hidden"
 >
+  <div class="cursor">
+    <div class="cursor__inner cursor__inner--circle"></div>
+    <div class="cursor__inner cursor__inner--dot"></div>
+  </div>
   <Blur />
   <Hero />
   <Why />
@@ -31,3 +114,27 @@
   <Newsletter />
   <Faq />
 </div>
+
+<style>
+  .cursor__inner {	
+	z-index: 999999;
+	pointer-events: none;
+	position: absolute;
+	top: 0;
+	left: 0;
+/* 	mix-blend-mode: difference; */
+	border-radius: 50%;
+}
+
+.cursor__inner--dot {
+	width: 6px;
+	height: 6px;
+	background: #000;
+}
+
+.cursor__inner--circle {
+	width: 20px;
+	height: 20px;
+	border: 1px solid #000;
+}
+</style>
