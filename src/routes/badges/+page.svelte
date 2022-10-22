61d434707d652e49e80d9efa387a92bd631ddff2
Trishiraj <gamerprince.exp@gmail.com>
2022-10-22 12:04:57 +0530

    Fix uneven rounded corner for choose frame button
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>

--- src/routes/badges/+page.svelte
+++ src/routes/badges/+page.svelte
@@ -96,7 +96,7 @@
             <button
               type="button"
               on:click={() => (frameSelector = "speaker")}
-              class="relative -ml-px inline-flex items-center rounded-r-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-black focus:outline-none focus:ring-1 focus:ring-black"
+              class="relative -ml-px inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-black focus:outline-none focus:ring-1 focus:ring-black"
             >
               Speaker
             </button>
