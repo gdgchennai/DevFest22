c39a0bf313c6b95e262c057cc7434d7eed6ded22
Naveen S <naveen2aug@gmail.com>
2022-10-28 20:28:12 +0530

    Added assets, routes. Updated home page to add sponsorship info (#80)

---
+++ src/routes/sponsorship/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+  <title>DevFest Chennai - Sponsorship</title>
+  <meta name="description" content="DevFest Chennai Sponsorship" />
+</svelte:head>
+
+
+<meta http-equiv="refresh" content="0; url=/sponsorship.pdf" />
+
+
+<h3 style="text-align:center">In case the page does not redirect<a href = "/sponsorship.pdf"> Click Here</a> </h3>
