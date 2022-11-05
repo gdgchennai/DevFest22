7d8a79e638daed9b5eaf17fb70629a0ee32020fc
navhits <naveen.sunilkumar@freshworks.com>
2022-11-05 11:33:36 +0530

    New logos and minor updates to existing one

--- src/lib/section/sponsors/Sponsors.svelte
+++ src/lib/section/sponsors/Sponsors.svelte
@@ -32,8 +32,17 @@
   <div class="bg-white">
     <div class="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:px-8">
       <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-        <img src="/google-devs.svg" alt="Google Developers" />
+        <img src="/google-devs.svg" class="img-border" alt="Google Developers" />
       </div>
     </div>
   </div>
 </section>
+
+<style>
+  .img-border {
+    border: 1px solid #ddd;
+    border-radius: 10px;
+    padding: 20px;
+    background-color: #fff;
+  }
+</style>
