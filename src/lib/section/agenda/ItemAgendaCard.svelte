52ec33e23f46a0187ffc2c9c11d83057a491c636
Trishiraj <gamerprince.exp@gmail.com>
2022-11-11 20:36:25 +0530

    Finalise Day 1 Track 1 agenda
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>

--- src/lib/section/agenda/ItemAgendaCard.svelte
+++ src/lib/section/agenda/ItemAgendaCard.svelte
@@ -1,21 +1,99 @@
 <script lang="ts">
   export let session: string = '';
-  export let speaker: string = '';
-  export let startTime: string = '';
-  export let endTime: string = '';
-  let ariaLabel = `${session} by ${speaker} from ${startTime} to ${endTime}`;
+  export let name: string = '';
+  export let imageUrl: string = '';
+  export let imageUrl2: string = '';
+  export let imageUrl3: string = '';
+  export let showImage: boolean = false;
+  export let showMultipleImage: boolean = false;
+  export let time: string = '';
 </script>
 
-<div aria-label={ariaLabel}>
-  <div class="mx-auto mb-8 h-px w-48 bg-indigo-500/10" />
-  <h4 class="text-lg font-semibold tracking-tight text-gray-900">
-    {session}
-  </h4>
-  <p class="mt-1 tracking-tight text-gray-500">{speaker}</p>
-  <p class="mt-1 font-mono text-sm text-gray-500">
-    <time datetime="2022-04-04T9:00AM-08:00">{startTime}</time>
-    <!-- -->-<!-- -->
-    <time datetime="2022-04-04T10:00AM-08:00">{endTime}</time>
-    <!-- -->IST
-  </p>
+<div class="single_speaker">
+  <div class="flex lg:pl-8">
+  {#if showImage}
+  <img
+    class="h-24 w-24 rounded-3xl"
+    src={imageUrl}
+    alt={name}
+  />
+  {#if showMultipleImage}
+  <img
+    class="h-24 w-24 rounded-3xl"
+    src={imageUrl2}
+    alt={name}
+  />
+  <img
+    class="h-24 w-24 rounded-3xl"
+    src={imageUrl3}
+    alt={name}
+  />
+  {/if}
+  {/if}
+  </div>
+  <div class="speaker-name">
+    <div class="heading flex items-center justify-between">
+      <span>{name}</span>
+      <div class="time">{time}</div>
+    </div>
+    <p>{session}</p>
+  </div>
 </div>
+
+<style>
+  .single_speaker {
+    margin-bottom: 63px;
+  }
+
+  @media (max-width: 767px) {
+    .single_speaker {
+      margin-bottom: 30px;
+    }
+  }
+
+  .single_speaker img {
+    float: left;
+    margin-right: 0.5rem;
+  }
+
+  @media (max-width: 767px) {
+    .single_speaker img {
+      float: none;
+    }
+  }
+
+  .single_speaker .speaker-name {
+    padding-left: 30px;
+    overflow: hidden;
+  }
+
+  @media (max-width: 767px) {
+    .single_speaker .speaker-name {
+      padding-left: 0;
+      margin-top: 20px;
+    }
+  }
+
+  .single_speaker .speaker-name .heading {
+    margin-bottom: 5px;
+  }
+
+  .single_speaker .speaker-name .heading span {
+    font-size: 26px;
+    color: #1f1f1f;
+    font-weight: 500;
+  }
+
+  .single_speaker .speaker-name .heading .time {
+    font-size: 16px;
+    font-weight: 500;
+    color: #1f1f1f;
+  }
+
+  .single_speaker p {
+    color: #707070;
+    font-size: 16px;
+    line-height: 28px;
+  }
+
+</style>
--- src/routes/agenda/+page.svelte
+++ src/routes/agenda/+page.svelte
@@ -1,10 +1,121 @@
+<script>
+  import ItemAgendaCard from '$lib/section/agenda/ItemAgendaCard.svelte';
+
+  const dayOneAgenda = [
+    {
+      name: 'Registration',
+      session: 'First come first serve basis',
+      time: '8:00 AM - 8:55 AM'
+    },
+    {
+      name: 'Opening note',
+      session: 'Vanakkam to DevFest Chennai 2022!!',
+      time: '9:05 AM - 9:15 AM'
+    },
+    {
+      name: 'Manikandan Krishnamurthy',
+      session: 'Multi-team platform on GKE',
+      imageUrl: '/profile_pics/Manikandan_Krishnamurthy.png',
+      showImage: true,
+      time: '9:20 AM - 09:55 AM'
+    },
+    {
+      name: 'Anush Arvind, Arumugaraja Thirumani, Ritikesh G',
+      session: 'Scaling from 10k RPM to 150k RPM',
+      imageUrl: '/profile_pics/Anush_Aravind.png',
+      imageUrl2: '/profile_pics/Arumgaraja_Thirumani.png',
+      imageUrl3: '/profile_pics/Ritikesh_G.png',
+      showImage: true,
+      showMultipleImage: true,
+      time: '10:00 AM - 10:35 AM'
+    },
+    {
+      name: 'Praveen Thirumurugan',
+      session: 'Microservice orchestration with GCP\'s serverless offerings',
+      imageUrl: '/profile_pics/Praveen_Thirumurugan.png',
+      showImage: true,
+      time: '10:40 AM - 11:15 AM'
+    },
+    {
+      name: 'Arun Ganesh',
+      session: 'Streaming XR using edge cloud',
+      imageUrl: '/profile_pics/Arun_Ganesh.png',
+      showImage: true,
+      time: '11:20 AM - 11:55 AM'
+    },
+    {
+      name: 'Joinal Ahmed',
+      session: 'Take Data to the next level with Graph Machine Learning',
+      imageUrl: '/profile_pics/Joinal_Ahmed.png',
+      showImage: true,
+      time: '12:00 PM - 12:35 PM'
+    },
+    {
+      name: 'Vasudev Maduri',
+      session: 'Ensuring Reliability of Large Scale ML Systems',
+      imageUrl: '/profile_pics/Vasudev_Maduri.png',
+      showImage: true,
+      time: '12:40 PM - 1:15 PM'
+    },
+    {
+      name: 'Lunch',
+      session: 'Time to feast yourself and prepare for what\'s coming next',
+      time: '1:15 PM - 2:15 PM'
+    },
+    {
+      name: 'Abhishek Mishra',
+      session: 'An artless introduction to distributed SQL database',
+      imageUrl: '/profile_pics/Abishek_Mishra.png',
+      showImage: true,
+      time: '2:15 PM - 2:40 PM'
+    },
+    {
+      name: 'Anam Saatvik Reddy',
+      session: 'Lightning talk - TensorFlow Recommenders - a simple library for building powerful recommendation systems',
+      imageUrl: '/profile_pics/Anam_Saatvik_Reddy.png',
+      showImage: true,
+      time: '2:45 PM - 2:55 PM'
+    },
+    {
+      name: 'Karthikeyan S',
+      session: 'Lightning talk - Video calls Demystified; A peak into WebRTC',
+      imageUrl: '/profile_pics/Karthikeyan_S.png',
+      showImage: true,
+      time: '2:55 PM - 3:05 PM'
+    },
+    {
+      name: 'Ramachandran A',
+      session: 'Lightning talk - Scaling Web3 development with GCP',
+      imageUrl: '/profile_pics/Ramachandran_A.png',
+      showImage: true,
+      time: '3:05 PM - 3:20 PM'
+    },
+    {
+      name: 'Networking and Tea break',
+      session: 'Connect - Interact and expand your clan!',
+      time: '3:25 PM - 4:00 PM'
+    },
+    {
+      name: 'Fun activities/Quiz',
+      session: 'Enough lectures, It\'s play time!',
+      time: '4:05 PM - 4:40 PM'
+    },
+    {
+      name: 'Closing Note',
+      session: 'Thank you for sticking with us this far!',
+      time: '4:40 PM - 4:50 PM'
+    },
+  ];
+</script>
+
 <svelte:head>
   <title>GDG Chennai - Agenda</title>
   <meta name="Event agenda" content="GDG Chennai - Event agenda" />
 </svelte:head>
+
 <div class="flex flex-col justify-start space-y-10 overflow-hidden p-8">
   <h2
-    class="w-full text-center text-3xl font-bold tracking-tight sm:text-4xl lg:text-center lg:text-5xl py-8"
+    class="w-full py-8 text-center text-3xl font-bold tracking-tight sm:text-4xl lg:text-center lg:text-5xl"
   >
     Agenda
   </h2>
@@ -12,10 +123,23 @@
     <div class="container mx-auto">
       <div class="double_line">
         <div class="row">
-          <div class="date">
+          <div class="date mb-8">
             <h3>12 Nov 2022</h3>
+            <h2>Track 1</h2>
           </div>
-          <div class="single_speaker">
+          {#each dayOneAgenda as speaker}
+            <ItemAgendaCard
+              session={speaker.session}
+              name={speaker.name}
+              imageUrl={speaker.imageUrl}
+              imageUrl2={speaker.imageUrl2}
+              imageUrl3={speaker.imageUrl3}
+              time={speaker.time}
+              showImage={speaker.showImage}
+              showMultipleImage={speaker.showMultipleImage}
+            />
+          {/each}
+          <!-- <div class="single_speaker">
             <img
               class="h-24 w-24 rounded-3xl"
               src="/profile_pics/Vasudev_Maduri.png"
@@ -28,35 +152,7 @@
               </div>
               <p>Lorem ipsum cuz why not</p>
             </div>
-          </div>
-          <div class="single_speaker">
-            <img
-              class="h-24 w-24 rounded-3xl"
-              src="/profile_pics/Vasudev_Maduri.png"
-              alt=""
-            />
-            <div class="speaker-name">
-              <div class="heading flex items-center justify-between">
-                <span>John Doe</span>
-                <div class="time">12-1.00 pm</div>
-              </div>
-              <p>Lorem ipsum cuz why not</p>
-            </div>
-          </div>
-          <div class="single_speaker">
-            <img
-              class="h-24 w-24 rounded-3xl"
-              src="/profile_pics/Vasudev_Maduri.png"
-              alt=""
-            />
-            <div class="speaker-name">
-              <div class="heading flex items-center justify-between">
-                <span>Varun</span>
-                <div class="time">2.30-4.00 pm</div>
-              </div>
-              <p>Lorem ipsum cuz why not</p>
-            </div>
-          </div>
+          </div> -->
         </div>
       </div>
       <div class="row">
@@ -189,6 +285,12 @@
     font-size: 26px;
     font-weight: 500;
     color: #ff0000;
+  }
+
+  .event_area .date h2 {
+    font-size: 20px;
+    font-weight: 500;
+    color: #0022ff;
   }
 
   @media (max-width: 767px) {
