c3007d1be19d19df4ac85ea8719ef927170f30e6
Trishiraj <gamerprince.exp@gmail.com>
2022-11-12 01:01:31 +0530

    Add embedded youtube livestream section
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>

---
+++ src/lib/section/youtube/youtube.svelte
@@ -0,0 +1,25 @@
+<section id="why" class="flex w-full flex-col items-center space-y-6 px-24">
+  <div class="pt-12 sm:pt-16 lg:pt-20">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="text-center">
+        <h2
+          class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl lg:text-5xl"
+        >
+          Don't miss out on sessions!
+        </h2>
+        <p class="mt-4  text-gray-600 sm:text-xl">
+          You can watch all our amazing speaker sessions right at the comfort of your own place!
+        </p>
+      </div>
+      <div class="sm:flex">
+        <div class="max-w-sm rounded overflow-hidden shadow-lg m-5">
+          <iframe width="300" height="300" src="https://www.youtube.com/embed/coFbkjbuMdA?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+
+        <div class="max-w-sm rounded overflow-hidden shadow-lg m-5">
+          <iframe width="300" height="300" src="https://www.youtube.com/embed/coFbkjbuMdA?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
