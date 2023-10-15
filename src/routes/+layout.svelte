d12c7932a3ac7df82bae86bb61bceb61372a57b6
nammahari <hariprasanth581@gmail.com>
2023-10-15 11:10:09 +0530

    discord

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <svelte:head> 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <style>
     @import url('https://fonts.googleapis.com/css?family=Google+Sans:400,500&display=swap');
   </style>
