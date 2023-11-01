ac75e584978a6a9db7ffb2a8a3b20d4d94d6dafd
HARI PRASANTH S <hariprasanth581@gmail.com>
2023-11-01 12:43:25 +0530

    Update cfp.svelte

--- src/lib/section/CFP/cfp.svelte
+++ src/lib/section/CFP/cfp.svelte
@@ -13,7 +13,7 @@
           <h2
             class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl lg:text-5xl"
           >
-            Sumbit a Talk
+            Submit a Talk
           </h2>
           <p class="mt-4  text-gray-600 sm:text-xl">
             Your Voice, Your Stage: Let's Talk Innovation!</p>
