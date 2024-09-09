97e9eda8ef091d0f49ca8412ebcda90d47055d20
Dhanush Kandhan <128817931+dhanushk-offl@users.noreply.github.com>
2024-09-09 23:19:39 +0530

    Header Logo Padding Adjustments ✅
    Padding was adjusted for better logo view

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -70,7 +70,7 @@
         <a href="/">
           <span class="sr-only">GDG Chennai - Devfest 2024</span>
           <img
-            class="h-14 max-w-fit pt-1"
+            class="h-14 max-w-fit pt-2"
             src="/devfest24_logo.svg"
             alt="GDG Chennai - Devfest 2024"
           />
