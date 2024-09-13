705c906c7abd5b843d9fbc9fe05beeb14dbf0e8e
Dhanush Kandhan <128817931+dhanushk-offl@users.noreply.github.com>
2024-09-14 00:49:42 +0530

    Included Icon Interface for Feature List ✨

--- src/lib/section/tickets/Tickets.svelte
+++ src/lib/section/tickets/Tickets.svelte
@@ -1,22 +1,19 @@
 <script lang="ts">
-  import Button from '$lib/components/Button.svelte';
-  import Icon from '$lib/icon/Icon.svelte';
-
   function handleClick() {
-    location.href='/agenda'
+    location.href = '/agenda';
   }
 
   function download() {
-    window.open('schedule.pdf')
+    window.open('schedule.pdf');
   }
 
   const includedFeatures = [
-    'Scrumptious food',
-    'Exciting sessions',
-    'Networking opportunities',
-    'Take home cool swags',
-    'Chance to take part in fun activities',
-    'Meet expert speakers'
+    { feature: 'Scrumptious food', icon: '/icons/Fast food.svg' },
+    { feature: 'Exciting sessions', icon: '/icons/Conference.svg' },
+    { feature: 'Networking opportunities', icon: '/icons/People.svg' },
+    { feature: 'Take home cool swags', icon: '/icons/Birthday.svg' },
+    { feature: 'Chance to take part in fun activities', icon: '/icons/Fun glasses.svg' },
+    { feature: 'Meet expert speakers', icon: '/icons/User.svg' }
   ];
 </script>
 
@@ -25,9 +22,7 @@
     <div class="pt-12 sm:pt-16 lg:pt-20">
       <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="text-center">
-          <h2
-            class="text-3xl font-bold tracking-tight text-black sm:text-4xl lg:text-5xl"
-          >
+          <h2 class="text-3xl font-bold tracking-tight text-black sm:text-4xl lg:text-5xl">
             This is the best conference you will ever attend!
           </h2>
         </div>
@@ -36,26 +31,10 @@
     <div class="mt-8 bg-white pb-16 sm:mt-12 sm:pb-20 lg:pb-28">
       <div class="relative">
         <div class="absolute inset-0 h-1/2 bg-white" />
-        <div class="relative mx-auto max-w-7xl ">
-          <div
-            class="mx-auto max-w-lg overflow-hidden rounded-lg shadow-lg lg:flex lg:max-w-none"
-          >
+        <div class="relative mx-auto max-w-7xl">
+          <div class="mx-auto max-w-lg overflow-hidden rounded-lg shadow-lg lg:flex lg:max-w-none">
             <div class="flex-1 border border-gray-50 px-6 py-8 lg:p-12">
-              <div
-                class="absolute -bottom-4 right-3 flex h-8 w-full animate-pulse items-center justify-center overflow-hidden"
-              >
-                <div class="-mb-px flex h-[2px] w-80 -scale-x-100">
-                  <div
-                    class="w-full flex-none blur-sm [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"
-                  />
-                  <div
-                    class="-ml-[100%] w-full flex-none blur-[1px] [background-image:linear-gradient(90deg,rgba(56,189,248,0)_0%,#0EA5E9_32.29%,rgba(236,72,153,0.3)_67.19%,rgba(236,72,153,0)_100%)]"
-                  />
-                </div>
-              </div>
-              <h3
-                class="font-bold text-gray-900 sm:text-2xl sm:tracking-tight md:text-4xl"
-              >
+              <h3 class="font-bold text-gray-900 sm:text-2xl sm:tracking-tight md:text-4xl">
                 DevFest 2024
               </h3>
               <p class="mt-6 text-base text-gray-500">
@@ -65,20 +44,16 @@
               </p>
               <div class="mt-8">
                 <div class="flex items-center">
-                  <h4
-                    class="flex-shrink-0 pr-4 text-base font-semibold text-gray-900"
-                  >
+                  <h4 class="flex-shrink-0 pr-4 text-base font-semibold text-gray-900">
                     What can you expect?
                   </h4>
                   <div class="flex-1 border-t border-gray-100" />
                 </div>
-                <ul
-                  class="mt-8 space-y-5 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-5 lg:space-y-0"
-                >
-                  {#each includedFeatures as feature}
+                <ul class="mt-8 space-y-5 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-5 lg:space-y-0">
+                  {#each includedFeatures as { feature, icon }}
                     <li class="flex items-start lg:col-span-1">
                       <div class="flex-shrink-0">
-                        <Icon width="20" height="20" name="check" />
+                        <img src={icon} alt={feature} class="w-5 h-5" />
                       </div>
                       <p class="ml-3 text-sm text-gray-700">{feature}</p>
                     </li>
@@ -86,20 +61,18 @@
                 </ul>
               </div>
             </div>
-            <div
-              class="bg-gray-50 py-8 px-6 text-center lg:flex lg:flex-shrink-0 lg:flex-col lg:justify-center lg:p-12"
-            >
+            <div class="bg-gray-50 py-8 px-6 text-center lg:flex lg:flex-shrink-0 lg:flex-col lg:justify-center lg:p-12">
               <p class="text-4xl font-medium text-gray-900">
                 Know your Speakers & Schedule
               </p>
               <div class="mt-6 mx-10">
                 <p class="mt-6 text-base text-gray-500">
-                  Will be Anounced Soon
+                  Will be Announced Soon
                 </p>
               </div>
               <div class="mt-4 text-left text-xs">
                 <p class="font-light text-gray-900">
-                  <b class="font-semibold">Note:</b> Prior registration is required to attend the event.<br /> 
+                  <b class="font-semibold">Note:</b> Prior registration is required to attend the event.<br />
                   Entry is allowed only with a valid ticket.
                 </p>
               </div>
@@ -109,7 +82,7 @@
                 </p>
                 <ul class="font-light">
                   <li>
-                    October 5, 2024 @ IIT-M Research Park,Chennai
+                    October 5, 2024 @ IIT-M Research Park, Chennai
                   </li>
                 </ul>
               </div>
