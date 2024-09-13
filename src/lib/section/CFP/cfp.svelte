d47eaa069c6a597a7fbd4771cbac7c0a8c28c5a5
Dhanush Kandhan <128817931+dhanushk-offl@users.noreply.github.com>
2024-09-14 02:23:12 +0530

    CFP Closed, Agenda Soon Added ✅

--- src/lib/section/CFP/cfp.svelte
+++ src/lib/section/CFP/cfp.svelte
@@ -1,37 +1,29 @@
-<script lang="ts">
-    import Button from '$lib/components/Button.svelte';
-  
-    // open the sponsor form in a new tab
-    const opneCfp = () => {
-      window.open('http://bit.ly/speakers-assemble', '_blank');
-    };
-  </script>
+
 <section id="CFP" class="flex w-full flex-col items-center space-y-6 px-24">
-    <div class="pt-12 sm:pt-16 lg:pt-20">
-      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div class="text-center">
-          <h2
-            class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl lg:text-5xl"
-          >
-          Call for Speakers
-          </h2>
-          <p class="mt-4  text-gray-600 sm:text-xl">
-            Got a groundbreaking idea or tech innovation to share? We got just the platform for you to<br>
-		        join us as a speaker and make an impact!
-          </p>
-        </div>
+  <div class="pt-12 sm:pt-16 lg:pt-20">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="text-center">
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl lg:text-5xl">
+          Agenda & Speakers
+        </h2>
       </div>
     </div>
-    <div class="max-w-7xl px-4 sm:px-6 md:w-1/2">
-        <div class="mt-6 text-center">
-          <Button
-            id="CFP-Link"
-            title="Submit your proposal"
-            isSecondaryButton={false}
-            onClick={() => opneCfp()}
-          />
-        </div>
-      </div>
-
-  </section>
-  
+  </div>
+      <p class="mt-6 text-base text-gray-500">
+        Will be Announced Soon
+      </p>
+    <div class="mt-4 text-left text-xs">
+      <p class="font-light text-gray-900">
+        <b class="font-semibold">Note:</b> Prior registration is required to attend the event.<br />
+        Entry is allowed only with a valid ticket.
+      </p>
+    </div>
+    <div class="mt-4 text-left text-sm">
+      <p class="font-light text-gray-900">
+        <b class="font-semibold">Event info:</b>
+      </p>
+      <ul class="font-light">
+        <li>October 5, 2024 @ IIT-M Research Park, Chennai</li>
+      </ul>
+    </div>
+</section>
