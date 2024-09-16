ffb7720d7f46fad1c26032c0f2c6ea363bbb031a
Dhanush Kandhan <128817931+dhanushk-offl@users.noreply.github.com>
2024-09-16 14:19:19 +0530

    Re-Designed Borders 🎨✨

--- src/lib/section/coporate Partners/corporate.svelte
+++ src/lib/section/coporate Partners/corporate.svelte
@@ -7,49 +7,42 @@
   >
     Corporate Partners
   </h2>
-  <div class="bg-white">
+  <div class="bg-white w-full">
     <div class="w-full max-w-7xl py-12 px-4 sm:px-6 lg:px-8">
       <div class="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-4">
         <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-          <a href="https://www.jetbrains.com/">
-            <img
-              src="/jetbrains.svg"
-              class="img-border"
-              alt="Jet Brains"
-              height={50}
-              width={200}
-              loading="lazy"
-            />
+          <a href="https://www.jetbrains.com/" class="inline-block relative transition-all duration-300 ease-in-out hover:-translate-y-0.5 hover:-translate-x-0.5 group">
+            <div class="bg-[#ffce46] border-2 border-black rounded-lg shadow-[3px_3px_0_0_#000000] p-0.5 transition-shadow duration-300 ease-in-out group-hover:shadow-[6px_6px_0_0_#000000]">
+              <div class="bg-white border-2 border-black rounded-md p-4">
+                <img
+                  src="/jetbrains.svg"
+                  alt="Jet Brains"
+                  height={50}
+                  width={200}
+                  loading="lazy"
+                  class="block max-w-full h-auto"
+                />
+              </div>
+            </div>
           </a>
         </div>
         <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-          <a href="https://konfhub.com/">
-            <img
-              src="/konfhub.svg"
-              class="img-border"
-              alt="Konfhub"
-              height={50}
-              width={200}
-              loading="lazy"
-            />
+          <a href="https://konfhub.com/" class="inline-block relative transition-all duration-300 ease-in-out hover:-translate-y-0.5 hover:-translate-x-0.5 group">
+            <div class="bg-[#FFCF46] border-2 border-black rounded-lg shadow-[3px_3px_0_0_#000000] p-0.5 transition-shadow duration-300 ease-in-out group-hover:shadow-[5px_5px_0_0_#000000]">
+              <div class="bg-white border-2 border-black rounded-md p-4">
+                <img
+                  src="/konfhub.svg"
+                  alt="Konfhub"
+                  height={50}
+                  width={200}
+                  loading="lazy"
+                  class="block max-w-full h-auto"
+                />
+              </div>
+            </div>
           </a>
         </div>
       </div>
     </div>
   </div>
 </section>
-
-<style>
-  .img-border {
-    border: 1px solid #ddd;
-    border-radius: 10px;
-    padding: 20px;
-    background-color: #fff;
-  }
-
-  @media (min-width: 1024px) {
-    .lg\:grid-cols-4 {
-      grid-template-columns: repeat(4, minmax(0, 1fr));
-    }
-  }
-</style>
